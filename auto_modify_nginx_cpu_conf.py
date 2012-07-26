f8ac32e63dcdc5ce5eb3208156ecaca7b348da69
ming <moonbingbing@gmail.com>
2012-07-26 11:54:30 +0800

    complete the main function

--- auto_modify_nginx_cpu_conf.py
+++ auto_modify_nginx_cpu_conf.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 __version__ = '0.1'
 __author__  = 'moonbingbing at gmail.com'
 
 import os
+import re
 try:
     import multiprocessing #new in python 2.6
 except ImportError:
@@ -38,15 +38,29 @@ def auto_modify_nginx_cpu_conf(filepath = '/etc/nginx.conf'):
         return False
     
     nginx_conf = ''
-    try:
-        f = open(filepath)
+    with open(filepath) as f:
         nginx_conf = f.read()
-    finally:
-        f.close()
         
     cpu_processor_count = get_cpu_processor_count()
     worker_cpu_affinity = get_worker_cpu_affinity(cpu_processor_count)
     
-    
+    cpu_affinity_conf = 'worker_cpu_affinity %s;' % worker_cpu_affinity
+    p = re.compile('worker_cpu_affinity[\d\s]+?;')
+    nginx_conf, sub_number = p.subn(cpu_affinity_conf, nginx_conf)
+    #if worker_cpu_affinity is not yet configured, auto add to the begin of nginx.conf
+    if sub_number == 0:
+        nginx_conf = cpu_affinity_conf + '\r\n' + nginx_conf
+        
+    cpu_processes_conf = 'worker_processes %s;' % cpu_processor_count
+    p = re.compile('worker_processes[\d\s]+?;')
+    nginx_conf, sub_number = p.subn(cpu_processes_conf, nginx_conf)
+    if sub_number == 0:
+        nginx_conf = cpu_processes_conf + '\r\n' + nginx_conf
+        
+    with open(filepath, 'w') as f:
+         f.write(nginx_conf)
+         
+    return True
+
 if __name__ == "__main__":
-    print get_cpu_processor_count()#worker_cpu_affinity(24)
+    print auto_modify_nginx_cpu_conf()
